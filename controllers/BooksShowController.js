97ba98caef2376be9bdbfcba75f5a6050a215028
jabad28 <abad.joanne@yahoo.com>
2016-04-16 16:11:45 -0700

    GET method in BooksShowController

--- controllers/BooksShowController.js
+++ controllers/BooksShowController.js
@@ -1,7 +1,20 @@
 angular.module('libraryApp')
-  .controller('BooksIndexController', BooksIndexController);
+  .controller('BooksShowController', BooksShowController);
 
 BooksShowController.$inject=['$http', '$routeParams', '$location'];
 function BooksShowController($http, $routeParams, $location) {
   var vm = this;
-};
+  var bookId = this;
+  $http({
+    method: 'GET',
+    url: 'https://super-crud.herokuapp.com/books/' + bookId
+  }).then(BookShowSucess, BookShowError);
+
+  function BookShowSucess(response){
+    console.log('book show data', bookId, response.data);
+    vm.book = response.data;
+  }
+  function BookShowError(error){
+    console.log('book show error', error);
+  }
+}
