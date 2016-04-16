cf5e5511694ea666b03e6183f0cd13cbcbef7e24
jabad28 <abad.joanne@yahoo.com>
2016-04-16 12:01:53 -0700

    books id in app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-angular.module('libraryApp', ['ngRoute'])
+angular
+       .module('libraryApp', ['ngRoute'])
        .config(config)
        .controller('BooksShowController', BooksShowController);
 
@@ -6,15 +7,21 @@ angular.module('libraryApp', ['ngRoute'])
 // ROUTES //
 ////////////
 
+
+
 config.$inject = ['$routeProvider', '$locationProvider'];
 function config (  $routeProvider,   $locationProvider  )  {
   $routeProvider
     .when('/', {
-      templateUrl: /* Include the path to the index template */,
-      controller:  /* Which controller do you want the main page to use */,
-      controllerAs:/* What will you call the controller in the html? */
+      templateUrl: "/templates/books/index.html",
+      controller:  "BooksIndexController",
+      controllerAs: "BooksShowCtrl"
     })
-    /* Include the additional route here! */
+    .when('/books/:id', {
+      templateUrl: "/templates/books/show.html",
+      controller:  "BooksShowController",
+      controllerAs: "BooksShowCtrl"
+    })
     .otherwise({
       redirectTo: '/'
     });
@@ -25,4 +32,4 @@ function config (  $routeProvider,   $locationProvider  )  {
       enabled: true,
       requireBase: false
     });
-};
+}
