b0e2adcdf73066c64c59f557bbc32980eefafb2e
jabad28 <abad.joanne@yahoo.com>
2016-04-17 12:17:59 -0700

    edited BooksShowController.js and show. html

--- controllers/BooksShowController.js
+++ controllers/BooksShowController.js
@@ -17,4 +17,27 @@ function BooksShowController($http, $routeParams, $location) {
   function BookShowError(error){
     console.log('book show error', error);
   }
+
+  vm.updateBook = function (book) {
+    $http({
+      method: 'PUT',
+      url: 'https://super-crud.herokuapp.com/books/'+ book._id,
+    }).then(function bookUpdatesuccessCallback(json) {
+      // don't need to do anything!
+    }, function errorCallback(response) {
+      console.log('There was an error editing the data', response.data);
+    });
+  };
+
+  vm.deleteBook = function (book) {
+    $http({
+      method: 'DELETE',
+      url: 'https://super-crud.herokuapp.com/books/'+ book._id,
+    }).then(function bookDeletesuccessCallback(json) {
+      var index = vm.books.indexOf(book);
+      // vm.albums.splice(index,1);
+    }, function errorCallback(response) {
+      console.log('There was an error deleting the data', response.data);
+    });
+  };
 }
